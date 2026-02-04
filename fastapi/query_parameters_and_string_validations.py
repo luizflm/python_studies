8158643fd53123ab46a38e77c45d3a63357bf0a5
Luiz Felipe <lepinholima@gmail.com>
2026-02-04 15:05:33 -0300

    query parameters and string validations

---
+++ fastapi/query_parameters_and_string_validations.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+            max_length=50
+        )
+    ]
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
